cd23e2e3088efcf83e395d2bb4a31a8a2bf46d60
Sebastian Manzano <jose.eustacio.eliberto@gmail.com>
2025-06-16 17:28:58 -0500

    agregue scopeinclude y scopefilter a category, comment, quedo funcionado

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+        use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
@@ -16,6 +19,7 @@ class Comment extends Model
         'blog_id',
         'user_id',
     ];
+    protected $allowFilter = ['id', 'content', 'blog_id', 'user_id',];
 
     /**
      * Define la relación con el modelo Blog.
@@ -38,4 +42,43 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeIncluded($query)
+    {
+        $allowedIncludes = ['blog','user'];
+
+        if (request()->has('include')) {
+            $includes = explode(',', request('include'));
+            $includes = array_intersect($includes, $allowedIncludes);
+            $query->with($includes);
+        }
+
+        return $query;
+    }   
+
+    public function scopeFilter(Builder $query)
+    {
+
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+
+            if ($allowFilter->contains($filter)) {
+
+                $query->where($filter, 'LIKE', '%' . $value . '%');//nos retorna todos los registros que conincidad, asi sea en una porcion del texto
+            }
+        }
+
+
+
+    }
+
+    
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users',[ApiController::class, 'index'])->name('api.users.index');
+
+
+
+
+
+
+
+
+Route::resource('categories',CategoryController::class);
+Route::resource('comments',CommentController::class);
+Route::resource('comments',CommentController::class);
 
 
