05c90542e7c76bd323f2011ecf95eedf32136341
Sebastian Manzano <jose.eustacio.eliberto@gmail.com>
2025-09-23 17:05:53 -0500

    estan todos los modelos

---
+++ app/Models/Blog.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $allowFilter = ['id', 'title', 'content', 'image_pach'];
+    protected $allowSort = ['id', 'title', 'content', 'image_pach'];
+
+    protected $fillable = [
+        'title',        // Título del blog
+        'content',      // Contenido del blog
+        'image_path',   // Ruta de la imagen asociada al blog
+        'category_id',  // ID de la categoría a la que pertenece el blog
+        'user_id',      // ID del usuario que creó el blog
+        // otros campos que desees permitir para asignación masiva
+    ];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'blog_tag', 'blog_id', 'tag_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function scopeIncluded($query)
+    {
+        $allowedIncludes = ['category', 'user', 'comments', 'tags', 'likes'];
+
+
+        if (request()->has('include')) {
+            $includes = explode(',', request('include'));
+            $includes = array_intersect($includes, $allowedIncludes);
+            $query->with($includes);
+        }
+
+        return $query;
+    }
+
+    public function scopeFilter(Builder $query)
+    {
+
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+
+            if ($allowFilter->contains($filter)) {
+
+                $query->where($filter, 'LIKE', '%' . $value . '%');//nos retorna todos los registros que conincidad, asi sea en una porcion del texto
+            }
+        }
+
+    }
+
+    public function scopeSort(Builder $query)
+    {
+
+     if (empty($this->allowSort) || empty(request('sort'))) {
+            return;
+        }
+
+        $sortFields = explode(',', request('sort'));
+        $allowSort = collect($this->allowSort);
+
+      foreach ($sortFields as $sortField) {
+
+            $direction = 'asc';
+
+            if(substr($sortField, 0,1)=='-'){ //cambiamos la consulta a 'desc'si el usuario antecede el menos (-) en el valor de la variable sort
+                $direction = 'desc';
+                $sortField = substr($sortField,1);//copiamos el valor de sort pero omitiendo, el primer caracter por eso inicia desde el indice 1
+            }
+            if ($allowSort->contains($sortField)) {
+                $query->orderBy($sortField, $direction);//ejecutamos la query con la direccion deseada sea 'asc' o 'desc'
+            }
+        }
+        //http://api.blog.test/v1/categories?sort=name
+    }
+
+    public function scopeGetOrPaginate(Builder $query)
+    {
+      if (request('perPage')) {
+            $perPage = intval(request('perPage'));//transformamos la cadena que llega en un numero.
+
+            if($perPage){//como la funcion intval retorna 0 si no puede hacer la conversion 0  es = false
+                return $query->paginate($perPage);//retornamos la cuonsulta de acuerdo a la ingresado en la vaiable $perPage
+            }
+
+
+         }
+           return $query->get();//sino se pasa el valor de $perPage en la URL se pasan todos los registros.
+        //http://api.codersfree1.test/v1/categories?perPage=2
+    }
+
+    
+}
---
+++ app/Models/Like.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+     protected $fillable = [
+        'user_id',
+        'blog_id',
+    ];
+        protected $allowFilter = ['id', 'blog_id', 'user_id',];
+        protected $allowSort = ['id', 'blog_id', 'user_id',];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function blog()
+    {
+        return $this->belongsTo(Blog::class);
+    }
+
+    public function scopeIncluded($query)
+    {
+        $allowedIncludes = ['user', 'blog'];
+
+        if (request()->has('include')) {
+            $includes = explode(',', request('include'));
+            $includes = array_intersect($includes, $allowedIncludes);
+            $query->with($includes);
+        }
+
+        return $query;
+    }
+
+    public function scopeFilter(Builder $query)
+    {
+
+        if (empty($this->allowFilter) || empty(request('filter'))) {
+            return;
+        }
+
+        $filters = request('filter');
+
+        $allowFilter = collect($this->allowFilter);
+
+        foreach ($filters as $filter => $value) {
+
+            if ($allowFilter->contains($filter)) {
+
+                $query->where($filter, 'LIKE', '%' . $value . '%');//nos retorna todos los registros que conincidad, asi sea en una porcion del texto
+            }
+        }
+
+    }
+
+    public function scopeSort(Builder $query)
+    {
+
+     if (empty($this->allowSort) || empty(request('sort'))) {
+            return;
+        }
+
+        $sortFields = explode(',', request('sort'));
+        $allowSort = collect($this->allowSort);
+
+      foreach ($sortFields as $sortField) {
+
+            $direction = 'asc';
+
+            if(substr($sortField, 0,1)=='-'){ //cambiamos la consulta a 'desc'si el usuario antecede el menos (-) en el valor de la variable sort
+                $direction = 'desc';
+                $sortField = substr($sortField,1);//copiamos el valor de sort pero omitiendo, el primer caracter por eso inicia desde el indice 1
+            }
+            if ($allowSort->contains($sortField)) {
+                $query->orderBy($sortField, $direction);//ejecutamos la query con la direccion deseada sea 'asc' o 'desc'
+            }
+        }
+        //http://api.blog.test/v1/categories?sort=name
+    }
+
+     public function scopeGetOrPaginate(Builder $query)
+    {
+      if (request('perPage')) {
+            $perPage = intval(request('perPage'));//transformamos la cadena que llega en un numero.
+
+            if($perPage){//como la funcion intval retorna 0 si no puede hacer la conversion 0  es = false
+                return $query->paginate($perPage);//retornamos la cuonsulta de acuerdo a la ingresado en la vaiable $perPage
+            }
+
+
+         }
+           return $query->get();//sino se pasa el valor de $perPage en la URL se pasan todos los registros.
+        //http://api.codersfree1.test/v1/categories?perPage=2
+    }   
+}
