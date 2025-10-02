9d4ba6b0a73d199b06e01dadcc147b6bc27b9c64
Guillermo Gomez <cducmc@gmail.com>
2025-10-02 17:23:17 -0500

    rutas api

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,13 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('users', UserController::class)->names('api.users');
-Route::resource('blogs', BlogController::class)->names('api.blogs');
-Route::resource('categories', CategoryController::class)->names('api.categories');
-Route::resource('comments', CommentController::class)->names('api.comments');
-Route::resource('tags', TagController::class)->names('api.tags');
-Route::resource('likes', LikeController::class)->names('api.likes');
+Route::resource('users', UserController::class);
+Route::resource('blogs', BlogController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('comments', CommentController::class);
+Route::resource('tags', TagController::class);
+Route::resource('likes', LikeController::class);
 
 // Ruta personalizada para unlike
 Route::delete('likes/{blog_id}/user/{user_id}', [LikeController::class, 'destroyByBlogAndUser'])
-    ->name('api.likes.destroyByBlogAndUser');
+    ->name('likes.destroyByBlogAndUser');
